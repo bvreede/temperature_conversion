187b0af14286c042abbea167da92058bfaa4e119
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-06-20 15:23:07 +0200

    refactor exercise to use conversion functions

--- exercise.py
+++ exercise.py
@@ -1,48 +1,21 @@
+import conversion
+
+
 def convert_temperature(temperature, unit):
     if _check_valid_temperature(temperature, unit) is False:
         return "Invalid temperature"
     if unit == "F":
-        # Convert Fahrenheit to Celsius
-        celsius = (temperature - 32) * (5 / 9)
-        # Convert Celsius to Kelvin
-        kelvin = celsius + 273.15
-        if celsius % 2 == 0:
-            # Convert Celsius to Fahrenheit
-            fahrenheit = (celsius * (9 / 5)) + 32
-            if fahrenheit % 2 == 0:
-                return fahrenheit, kelvin
-            else:
-                return fahrenheit, celsius
-        else:
-            return celsius, kelvin
+        kelvin = conversion.fahrenheit_to_kelvin(temperature)
+        celsius = conversion.fahrenheit_to_celsius(temperature)
+        return celsius, kelvin
     elif unit == "C":
-        # Convert Celsius to Fahrenheit
-        fahrenheit = (temperature * (9 / 5)) + 32
-        # Convert Celsius to Kelvin
-        kelvin = temperature + 273.15
-        if temperature % 2 == 0:
-            # Convert Celsius to Fahrenheit
-            fahrenheit = (temperature * (9 / 5)) + 32
-            if fahrenheit % 2 == 0:
-                return fahrenheit, kelvin
-            else:
-                return fahrenheit, temperature
-        else:
-            return fahrenheit, kelvin
+        kelvin = conversion.celsius_to_kelvin(temperature)
+        fahrenheit = conversion.celsius_to_fahrenheit(temperature)
+        return fahrenheit, kelvin
     elif unit == "K":
-        # Convert Kelvin to Celsius
-        celsius = temperature - 273.15
-        # Convert Celsius to Fahrenheit
-        fahrenheit = (celsius * (9 / 5)) + 32
-        if celsius % 2 == 0:
-            # Convert Celsius to Fahrenheit
-            fahrenheit = (celsius * (9 / 5)) + 32
-            if fahrenheit % 2 == 0:
-                return fahrenheit, celsius
-            else:
-                return fahrenheit, kelvin
-        else:
-            return celsius, fahrenheit
+        celsius = conversion.kelvin_to_celsius(temperature)
+        fahrenheit = conversion.kelvin_to_fahrenheit(temperature)
+        return celsius, fahrenheit
     else:
         return "Invalid unit"
 
@@ -55,3 +28,4 @@ def _check_valid_temperature(temperature, unit):
         return False
     if unit == "K" and temperature < 0:
         return False
+    return True
