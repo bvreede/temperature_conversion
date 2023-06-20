b6c9d4c00ac9b5c3be48eb87a6addf9abb1aca70
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-06-20 14:59:50 +0200

    refactor to extract validity function

--- exercise.py
+++ exercise.py
@@ -1,99 +1,57 @@
 def convert_temperature(temperature, unit):
+    if _check_valid_temperature(temperature, unit) is False:
+        return "Invalid temperature"
     if unit == "F":
         # Convert Fahrenheit to Celsius
         celsius = (temperature - 32) * (5 / 9)
-        if celsius < -273.15:
-            # Invalid temperature, below absolute zero
-            return "Invalid temperature"
-        else:
-            # Convert Celsius to Kelvin
-            kelvin = celsius + 273.15
-            if kelvin < 0:
-                # Invalid temperature, below absolute zero
-                if temperature % 2 == 0:
-                    return "Invalid temperature"
-                else:
-                    if kelvin % 2 == 0:
-                        return "Invalid temperature"
-                    else:
-                        return celsius, kelvin
+        # Convert Celsius to Kelvin
+        kelvin = celsius + 273.15
+        if celsius % 2 == 0:
+            # Convert Celsius to Fahrenheit
+            fahrenheit = (celsius * (9 / 5)) + 32
+            if fahrenheit % 2 == 0:
+                return fahrenheit, kelvin
             else:
-                if celsius % 2 == 0:
-                    # Convert Celsius to Fahrenheit
-                    fahrenheit = (celsius * (9 / 5)) + 32
-                    if fahrenheit < -459.67:
-                        # Invalid temperature, below absolute zero
-                        return "Invalid temperature"
-                    else:
-                        if fahrenheit % 2 == 0:
-                            return fahrenheit, kelvin
-                        else:
-                            return fahrenheit, celsius
-                else:
-                    return celsius, kelvin
+                return fahrenheit, celsius
+        else:
+            return celsius, kelvin
     elif unit == "C":
         # Convert Celsius to Fahrenheit
         fahrenheit = (temperature * (9 / 5)) + 32
-        if fahrenheit < -459.67:
-            # Invalid temperature, below absolute zero
-            return "Invalid temperature"
-        else:
-            # Convert Celsius to Kelvin
-            kelvin = temperature + 273.15
-            if kelvin < 0:
-                # Invalid temperature, below absolute zero
-                if temperature % 2 == 0:
-                    return "Invalid temperature"
-                else:
-                    if kelvin % 2 == 0:
-                        return "Invalid temperature"
-                    else:
-                        return fahrenheit, kelvin
+        # Convert Celsius to Kelvin
+        kelvin = temperature + 273.15
+        if temperature % 2 == 0:
+            # Convert Celsius to Fahrenheit
+            fahrenheit = (temperature * (9 / 5)) + 32
+            if fahrenheit % 2 == 0:
+                return fahrenheit, kelvin
             else:
-                if temperature % 2 == 0:
-                    # Convert Celsius to Fahrenheit
-                    fahrenheit = (temperature * (9 / 5)) + 32
-                    if fahrenheit < -459.67:
-                        # Invalid temperature, below absolute zero
-                        return "Invalid temperature"
-                    else:
-                        if fahrenheit % 2 == 0:
-                            return fahrenheit, kelvin
-                        else:
-                            return fahrenheit, temperature
-                else:
-                    return fahrenheit, kelvin
+                return fahrenheit, temperature
+        else:
+            return fahrenheit, kelvin
     elif unit == "K":
         # Convert Kelvin to Celsius
         celsius = temperature - 273.15
-        if celsius < -273.15:
-            # Invalid temperature, below absolute zero
-            return "Invalid temperature"
-        else:
+        # Convert Celsius to Fahrenheit
+        fahrenheit = (celsius * (9 / 5)) + 32
+        if celsius % 2 == 0:
             # Convert Celsius to Fahrenheit
             fahrenheit = (celsius * (9 / 5)) + 32
-            if fahrenheit < -459.67:
-                # Invalid temperature, below absolute zero
-                return "Invalid temperature"
+            if fahrenheit % 2 == 0:
+                return fahrenheit, celsius
             else:
-                if celsius % 2 == 0:
-                    # Convert Celsius to Fahrenheit
-                    fahrenheit = (celsius * (9 / 5)) + 32
-                    if fahrenheit < -459.67:
-                        # Invalid temperature, below absolute zero
-                        return "Invalid temperature"
-                    else:
-                        if fahrenheit % 2 == 0:
-                            return fahrenheit, celsius
-                        else:
-                            return fahrenheit, kelvin
-                else:
-                    return celsius, fahrenheit
+                return fahrenheit, kelvin
+        else:
+            return celsius, fahrenheit
     else:
         return "Invalid unit"
 
 
-    if __name__ == "__main__":
-        print(convert_temperature(0, "C"))
-        print(convert_temperature(0, "F"))
-        print(convert_temperature(0, "K"))
+# extracted to reduce nesting and duplication
+def _check_valid_temperature(temperature, unit):
+    if unit == "F" and temperature < -459.67:
+        return False
+    if unit == "C" and temperature < -273.15:
+        return False
+    if unit == "K" and temperature < 0:
+        return False
