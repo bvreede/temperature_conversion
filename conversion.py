6b3fe6cf730865c8d1236d45162ceb0fb3786106
Barbara Vreede <b.vreede@esciencecenter.nl>
2023-06-20 15:26:29 +0200

    reorder

--- conversion.py
+++ conversion.py
@@ -7,11 +7,11 @@ def celsius_to_kelvin(celsius):
 def fahrenheit_to_kelvin(fahrenheit):
     return (fahrenheit + 459.67) * 5/9.0
 
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32.0) * 5/9.0
+
 def kelvin_to_celsius(kelvin):
     return kelvin - 273.15
 
 def kelvin_to_fahrenheit(kelvin):
     return kelvin * 9/5.0 - 459.67
-
-def fahrenheit_to_celsius(fahrenheit):
-    return (fahrenheit - 32.0) * 5/9.0
